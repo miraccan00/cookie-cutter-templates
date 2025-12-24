2559497c76a77b1da95a090847012678e76d543b
Mirac Can Yilmaz <miraccanyilmazme@gmail.com>
2025-12-24 22:58:05 +0300

    Remove Java Spring Boot hello-api template files and configurations

--- cookiecutter-spring-mvn-template/hello-api/src/main/java/com/company/platform/hello/api/domain/service/GreetingService.java
+++
@@ -1,7 +0,0 @@
-package com.company.platform.hello.api.domain.service;
-
-import com.company.platform.hello.api.domain.model.Greeting;
-
-public interface GreetingService {
-    Greeting greet(String name);
-}
